15b269da2d859a3d7b48aa882780266744d9e7c4
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-01-02 12:55:48 +0330

    initial bug fixes for redux

--- src/components/features/todoSlice.js
+++ src/components/features/todoSlice.js
@@ -8,20 +8,19 @@ const todoSlice = createSlice({
   initialState,
   reducers: {
     addTodo: (state, action) => {
-        action.payload.e.preventDefault()
-        const newTodo = {
-            title:action.payload.title,
-            id:Date.now(),
-            completed:false
-        } 
-               state.todos.push(newTodo)
+      const newTodo = {
+        title: action.payload.title,
+        id: Date.now(),
+        completed: false,
+      };
+      state.todos.push(newTodo);
     },
     completeTodo: (state, action) => {
-        const todo = state.todos.find(t=>t.id==action.payload)
-        todo.completed = !todo.completed
+      const todo = state.todos.find((t) => t.id == action.payload);
+      todo.completed = !todo.completed;
     },
     deleteTodo: (state, action) => {
-       state.todos = state.todos.filter(t=>t.id!=action.payload)
+      state.todos = state.todos.filter((t) => t.id != action.payload);
     },
   },
 });
